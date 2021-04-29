07747f433027e85fbec1887ebb9f6f2205eb6b0c
KarolXX <karol@email.com>
2021-04-29 16:50:06 +0200

    frontend for displaying tasks and main menu

--- src/main/java/io/github/mat3e/todoapp/controller/ProjectController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/ProjectController.java
@@ -4,15 +4,15 @@ import io.github.mat3e.todoapp.logic.ProjectService;
 import io.github.mat3e.todoapp.model.Project;
 import io.github.mat3e.todoapp.model.ProjectStep;
 import io.github.mat3e.todoapp.model.projection.ProjectWriteModel;
+import org.flywaydb.core.internal.util.TimeFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -41,6 +41,7 @@ public class ProjectController {
         }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
         model.addAttribute("msg", "Project added!!!");
         return "projects";
     }
@@ -48,6 +49,23 @@ public class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("msg", "New group has been created");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("msg", "Error when creating group from this project");
+        }
+
         return "projects";
     }
 
